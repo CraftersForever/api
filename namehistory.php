93e2ae96e33b3f19c4788b494946f55ea5342fe1
BloodSKreaper <cochs9744@gmail.com>
2019-09-20 23:56:37 +0200

    Create namehistory.php
    A php script that returns the history of a Minecraft players usernames. Input may be the UUID or the username of the player. Supports CORS.
    
    A demo of this file can be viewed on https://api.craftersforever.de/names.php
    Working examples:
    - https://api.craftersforever.de/names.php?uuid=BloodSKreaper
    - https://api.craftersforever.de/names.php?uuid=14636f4d3ea94544a7837e6d14524a5c
    - https://api.craftersforever.de/names.php?uuid=14636f4d-3ea9-4544-a783-7e6d14524a5c

---
+++ namehistory.php
@@ -0,0 +1,31 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type:application/json");
+
+(isset($_GET['uuid'])&&$_GET['uuid'] != "") or die("There has to be either a UUID or username given");
+
+$uuid = $_GET['uuid'];
+if(strlen($uuid)<=16 && strlen($uuid)>=3){
+  $usernameRegex= "/[a-zA-Z0-9_]{3,16}/"; #According to https://help.mojang.com/customer/en/portal/articles/928638-minecraft-usernames?b_id=5408
+  preg_match($usernameRegex, $uuid) or die('{\"Error\":\"Invalid Username!\"}');
+  #Convert username too uuid
+  
+  $uuid = getUUIDFromMojang($uuid);
+}
+$uuid = preg_replace('/[\s-]/', '', $uuid);
+$UUIDRegex = '/[0-9a-f]{32}/';
+preg_match($UUIDRegex, $uuid) or die('{\"Error\":\"Invalid UUID!\"}');
+$url = "https://api.mojang.com/user/profiles/".$uuid."/names";
+$response = file_get_contents($url);
+print_r($response);
+
+
+
+function getUUIDFromMojang($username){
+	$url = "https://api.mojang.com/users/profiles/minecraft/".$username;
+	$obj = file_get_contents($url);
+	($obj != null) or die('{\"Error\":\"Mojang does not know that username!\"}');
+	$uuid = json_decode($obj)->id;
+  return $uuid;
+}
+?>
