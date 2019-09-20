600d744b356dbf6167b676cc824ab9e035ae1c57
BloodSKreaper <cochs9744@gmail.com>
2019-09-21 00:09:13 +0200

    Removed slashes from json output

--- namehistory.php
+++ namehistory.php
@@ -2,19 +2,19 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type:application/json");
 
-(isset($_GET['uuid'])&&$_GET['uuid'] != "") or die('{\"Error\":\"No Username or UUID given!\"}');;
+(isset($_GET['uuid'])&&$_GET['uuid'] != "") or die('{"Error":"No Username or UUID given!"}');;
 
 $uuid = $_GET['uuid'];
 if(strlen($uuid)<=16 && strlen($uuid)>=3){
   $usernameRegex= "/[a-zA-Z0-9_]{3,16}/"; #According to https://help.mojang.com/customer/en/portal/articles/928638-minecraft-usernames?b_id=5408
-  preg_match($usernameRegex, $uuid) or die('{\"Error\":\"Invalid Username!\"}');
+  preg_match($usernameRegex, $uuid) or die('{"Error":"Invalid Username!"}');
   #Convert username too uuid
   
   $uuid = getUUIDFromMojang($uuid);
 }
 $uuid = preg_replace('/[\s-]/', '', $uuid);
 $UUIDRegex = '/[0-9a-f]{32}/';
-preg_match($UUIDRegex, $uuid) or die('{\"Error\":\"Invalid UUID!\"}');
+preg_match($UUIDRegex, $uuid) or die('{"Error":"Invalid UUID!"}');
 $url = "https://api.mojang.com/user/profiles/".$uuid."/names";
 $response = file_get_contents($url);
 print_r($response);
@@ -24,7 +24,7 @@ print_r($response);
 function getUUIDFromMojang($username){
 	$url = "https://api.mojang.com/users/profiles/minecraft/".$username;
 	$obj = file_get_contents($url);
-	($obj != null) or die('{\"Error\":\"Mojang does not know that username!\"}');
+	($obj != null) or die('{"Error":"Mojang does not know that username!"}');
 	$uuid = json_decode($obj)->id;
   return $uuid;
 }
