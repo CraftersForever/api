e8a1a4471f9a2a0ee21674abf9c8e9342cd1fb0f
BloodSKreaper <cochs9744@gmail.com>
2020-02-04 14:59:35 +0100

    Update status.php

--- status.php
+++ status.php
@@ -1,61 +1,74 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 $servername = "localhost";
-$port = "3306";
-$database = "****"; #hidden for security reasons
-$username = "****"; #hidden for security reasons
-$password = "****"; #hidden for security reasons
+$port       = "3306";
+$database   = "****"; #hidden for security reasons
+$username   = "****"; #hidden for security reasons
+$password   = "****"; #hidden for security reasons
 
-$output = "";
-$conn = new mysqli($servername, $username, $password);
-if($conn -> connect_error){
+$conn   = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
     echo $conn->connect_error;
-    $output = "{\"Error\":\"Connection to Database failed\"}";
-}else{
-    mysqli_select_db($conn,$database);
-    $sql = "SELECT * FROM Status WHERE timestamp = (SELECT MAX(timestamp) as max FROM Status);";
-    $result = mysqli_query($conn, $sql);
-    if(!$result){
-        $output = "{\"Error\":\"SQL Error\"}";
-    }else{
-        $timestamp = 0;
-        $maxslots = 0;
-        $status = 0;
-        foreach($result as $row){
-            $timestamp = $row['timestamp'];
-            $maxslots = $row['slots'];
-            $status = $row['status'];
-        }
-
-        if(outdated($timestamp)){
-            $output = "{\"status\":\"offline\"}";
-        }else{
-            $onlinePlayers = array();
-            $sqlOnlinePlayers = "SELECT Spieler.Spieler_UUID, Spieler.Spieler_Name FROM OnlineSpieler INNER JOIN Spieler on OnlineSpieler.Spieler_UUID = Spieler.Spieler_UUID WHERE timestamp=".$timestamp.";";
-            $resultOnlinePlayers = mysqli_query($conn, $sqlOnlinePlayers);
-            foreach($resultOnlinePlayers as $resultOnlinePlayer){
-                $onlinePlayer = array(
-                    "uuid" => $resultOnlinePlayer['Spieler_UUID'],
-                    "name" => $resultOnlinePlayer['Spieler_Name']);
-                $onlinePlayers[] = $onlinePlayer;
-            }
-            $outputObject = array(
-                "status" => "online",
-	"slots" => $maxslots,
-	"players" => $onlinePlayers);
-	$output = json_encode($outputObject);
+    die('{\"Error\":\"Connection to Database failed\"}');
 }
+
+mysqli_select_db($conn, $database);
+$sql    = "SELECT
+  Status.timestamp,
+  Spieler.Spieler_UUID,
+  Spieler.Spieler_Name
+FROM
+  Status
+  LEFT JOIN OnlineSpieler on Status.timestamp = OnlineSpieler.timestamp
+  INNER JOIN Spieler on OnlineSpieler.Spieler_UUID = Spieler.Spieler_UUID
+WHERE
+  Status.timestamp = (
+    SELECT
+      MAX(Status.timestamp)
+    FROM
+      Status";
+$result = mysqli_query($conn, $sql);
+if (!$result) {
+    die('{"Error":"SQL Error"}');
+}
+$timestamp     = 0;
+$maxslots      = 0;
+$status        = 0;
+$onlinePlayers = array();
+
+foreach ($result as $row) {
+    $timestamp = $row['timestamp'];
+    $maxslots  = $row['slots'];
+    $status    = $row['status'];
+    if ($result['Spieler_UUID'] != null) {
+        $onlinePlayer    = array(
+            "uuid" => $result['Spieler_UUID'],
+            "name" => $result['Spieler_Name']
+        );
+        $onlinePlayers[] = $onlinePlayer;
     }
 }
-echo $output;
+
+if (outdated($timestamp)) {
+    die('{"status":"offline"}');
+}
+$outputObject = array(
+    "status" => "online",
+    "slots" => $maxslots,
+    "players" => $onlinePlayers
+);
+echo json_encode($outputObject);
 
 
-function outdated($entered){
-    $current = round(microtime(true)*1000);
-    $diff = $current - $entered;
-    if($diff >62000){
+
+
+function outdated($entered)
+{
+    $current = round(microtime(true) * 1000);
+    $diff    = $current - $entered;
+    if ($diff > 62000) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
